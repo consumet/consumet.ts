0f69c27c9f2497b2b38ce898fa869f06f41ce674
futaba <mythhb@yahoo.com>
2022-08-02 22:45:51 -0400

    fix anilist search

---
+++ dist/providers/meta/tmdb.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// class Tmdb extends MovieParser {
+//   override readonly name = 'FlixHQ';
+//   protected override baseUrl = 'https://flixhq.to';
+//   protected override logo =
+//     'https://img.flixhq.to/xxrz/400x400/100/ab/5f/ab5f0e1996cc5b71919e10e910ad593e/ab5f0e1996cc5b71919e10e910ad593e.png';
+//   protected override classPath = 'MOVIES.FlixHQ';
+//   protected override supportedTypes = new Set([TvType.MOVIE, TvType.TVSERIES]);
+//   /**
+//    *
+//    * @param query search query string
+//    * @param page page number (default 1) (optional)
+//    */
+//   override search = async (query: string, page: number = 1): Promise<ISearch<IMovieResult>> => {};
+//   /**
+//    *
+//    * @param mediaId media link or id
+//    */
+//   override fetchMediaInfo = async (mediaId: string): Promise<IMovieInfo> => {};
+//   /**
+//    *
+//    * @param episodeId episode id
+//    * @param mediaId media id
+//    * @param server server type (default `VidCloud`) (optional)
+//    */
+//   override fetchEpisodeSources = async () => {};
+//   /**
+//    *
+//    * @param episodeId takes episode link or movie id
+//    * @param mediaId takes movie link or id (found on movie info object)
+//    */
+//   override fetchEpisodeServers = async (episodeId: string, mediaId: string): Promise<IEpisodeServer[]> => {};
+// }
+//# sourceMappingURL=tmdb.js.map
