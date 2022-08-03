edabb79b671e53117baf3873396e84f33993e524
futaba <mythhb@yahoo.com>
2022-08-02 22:45:11 -0400

    fix anilist search

---
+++ src/providers/meta/tmdb.ts
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+import {
+  AnimeParser,
+  ISearch,
+  IAnimeInfo,
+  MediaStatus,
+  IAnimeResult,
+  ISource,
+  IAnimeEpisode,
+  SubOrSub,
+  IEpisodeServer,
+  MovieParser,
+} from '../../models';
+import { anilistSearchQuery, anilistMediaDetailQuery, kitsuSearchQuery } from '../../utils';
+import Gogoanime from '../../providers/anime/gogoanime';
+
+// class Tmdb extends MovieParser {
+//   override readonly name = 'FlixHQ';
+//   protected override baseUrl = 'https://flixhq.to';
+//   protected override logo =
+//     'https://img.flixhq.to/xxrz/400x400/100/ab/5f/ab5f0e1996cc5b71919e10e910ad593e/ab5f0e1996cc5b71919e10e910ad593e.png';
+//   protected override classPath = 'MOVIES.FlixHQ';
+//   protected override supportedTypes = new Set([TvType.MOVIE, TvType.TVSERIES]);
+
+//   /**
+//    *
+//    * @param query search query string
+//    * @param page page number (default 1) (optional)
+//    */
+//   override search = async (query: string, page: number = 1): Promise<ISearch<IMovieResult>> => {};
+
+//   /**
+//    *
+//    * @param mediaId media link or id
+//    */
+//   override fetchMediaInfo = async (mediaId: string): Promise<IMovieInfo> => {};
+
+//   /**
+//    *
+//    * @param episodeId episode id
+//    * @param mediaId media id
+//    * @param server server type (default `VidCloud`) (optional)
+//    */
+//   override fetchEpisodeSources = async () => {};
+
+//   /**
+//    *
+//    * @param episodeId takes episode link or movie id
+//    * @param mediaId takes movie link or id (found on movie info object)
+//    */
+//   override fetchEpisodeServers = async (episodeId: string, mediaId: string): Promise<IEpisodeServer[]> => {};
+// }
