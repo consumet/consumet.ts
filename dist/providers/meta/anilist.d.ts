97ce3788ab7ac7c2306c3c35c0bad9c1052ef5ba
futaba <mythhb@yahoo.com>
2022-08-09 13:09:42 -0400

    chore (anilist): cleanup

--- dist/providers/meta/anilist.d.ts
+++ dist/providers/meta/anilist.d.ts
@@ -39,6 +39,16 @@ declare class Anilist extends AnimeParser {
     private findAnimeSlug;
     fetchTrendingAnime: (page?: number, perPage?: number) => Promise<ISearch<IAnimeResult>>;
     fetchPopularAnime: (page?: number, perPage?: number) => Promise<ISearch<IAnimeResult>>;
+    /**
+     *
+     * @param page page number (optional)
+     * @param perPage number of results per page (optional)
+     * @param weekStart Filter by the time in epoch seconds (optional) eg. if you set weekStart to this week's monday, and set weekEnd to next week's sunday, you will get all the airing anime in between these two dates.
+     * @param weekEnd Filter by the time in epoch seconds (optional)
+     * @param notYetAired if true will return anime that have not yet aired (optional)
+     * @returns the next airing episodes
+     */
+    fetchAiringSchedule: (page?: number, perPage?: number, weekStart?: number, weekEnd?: number, notYetAired?: boolean) => Promise<ISearch<IAnimeResult>>;
     private findAnimeRaw;
 }
 export default Anilist;
