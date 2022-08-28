49631e2be32dc14ce118622c0528bfd100fdcf94
futaba <mythhb@yahoo.com>
2022-08-27 21:26:36 -0400

    chore: bump patch version `1.0.10` -> `1.1.0`

--- src/providers/anime/9anime.ts
+++ src/providers/anime/9anime.ts
@@ -25,6 +25,7 @@ class NineAnime extends AnimeParser {
   protected override logo =
     'https://d1nxzqpcg2bym0.cloudfront.net/google_play/com.my.nineanime/87b2fe48-9c36-11eb-8292-21241b1c199b/128x128';
   protected override classPath = 'ANIME.NineAnime';
+  override readonly isWorking = false;
 
   private readonly table = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
   private cipherKey = '';
