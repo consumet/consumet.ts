06fcc563e9f960d5da59d3a1c3cc8d4ce74453cb
InumakiEU <33759526+5H4D0WILA@users.noreply.github.com>
2022-11-26 02:44:45 +0100

    fix(mal): fixed search math (#116)

--- src/providers/meta/mal.ts
+++ src/providers/meta/mal.ts
@@ -112,7 +112,7 @@ class Myanimelist extends AnimeParser {
 
     const { data } = await axios.request({
       method: 'get',
-      url: `https://myanimelist.net/anime.php?q=${query}&cat=anime&show=${50 * page - 1}`,
+      url: `https://myanimelist.net/anime.php?q=${query}&cat=anime&show=${50 * (page - 1)}`,
       headers: {
         'user-agent':
           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35',
