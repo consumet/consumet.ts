10003a60ab78cf1e96c4daa7b10cdbde2ca8ea11
wearrrrr <rowancole19@gmail.com>
2022-12-06 14:54:46 -0600

    add gogoanime.fetchGenreInfo

--- test/anime/testing.ts
+++
@@ -1,9 +0,0 @@
-import { ANIME } from '../../src/providers';
-
-async function getGenreInfo() {
-    const gogoanime = new ANIME.Gogoanime();
-    const data = await gogoanime.fetchGenreInfo('action', 1)
-    console.log(data)
-}
-
-getGenreInfo()
