96ff789a7b6840cfe8823f3873dbe682d73c0484
Tdanks2000 <tommydanks2000@hotmail.com>
2022-12-16 14:19:02 +0000

    feat(MangaReader): Add MangaReader provider

--- src/providers/manga/index.ts
+++ src/providers/manga/index.ts
@@ -4,5 +4,6 @@ import MangaKakalot from './mangakakalot';
 import Mangasee123 from './mangasee123';
 import Mangapark from './mangapark';
 import MangaPill from './mangapill';
+import MangaReader from './mangareader';
 
-export default { MangaDex, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill };
+export default { MangaDex, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader };
