284e3b47571b758814dae20f4f5c39437fead135
Tdanks2000 <tommydanks2000@hotmail.com>
2022-12-25 22:50:47 +0000

    feat(dramacool) add dramacool provider

--- src/providers/movies/index.ts
+++ src/providers/movies/index.ts
@@ -1,4 +1,5 @@
 import FlixHQ from './flixhq';
 import ViewAsian from './viewAsian';
+import DramaCool from './dramacool';
 
-export default { FlixHQ, ViewAsian };
+export default { FlixHQ, ViewAsian, DramaCool };
