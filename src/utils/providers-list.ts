708d19e6d77e0dc6cfb1db0046c6cb7eef35ab1e
Tdanks2000 <40835340+TDanks2000@users.noreply.github.com>
2022-12-26 02:38:50 +0000

    feat(dramacool) add dramacool provider (#158)
    * feat(dramacool) add dramacool provider
    
    * [CodeFactor] Apply fixes to commit 284e3b4
    
    * fixedddd
    
    * change func to private
    
    Co-authored-by: Tdanks2000 <tommydanks2000@hotmail.com>
    Co-authored-by: codefactor-io <support@codefactor.io>

--- src/utils/providers-list.ts
+++ src/utils/providers-list.ts
@@ -28,7 +28,7 @@ export const PROVIDERS_LIST = {
   BOOKS: [new BOOKS.Libgen()],
   COMICS: [new COMICS.GetComics()],
   LIGHT_NOVELS: [new LIGHT_NOVELS.ReadLightNovels()],
-  MOVIES: [new MOVIES.FlixHQ(), new MOVIES.ViewAsian()],
+  MOVIES: [new MOVIES.FlixHQ(), new MOVIES.ViewAsian(), new MOVIES.DramaCool()],
   NEWS: [new NEWS.ANN()],
   META: [new META.Anilist(), new META.TMDB(), new META.Myanimelist()],
   OTHERS: [],
