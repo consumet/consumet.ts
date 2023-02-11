21d6277e40b3926128f6e6a1f025190997bdde66
InumakiEU <33759526+5H4D0WILA@users.noreply.github.com>
2023-02-11 15:32:50 +0100

    New ver (#250)
    * fixed RLN, animefox and added dub indicator to animepahe
    
    * removed async code used for testing
    
    * removed async code used for testing
    
    * v1.4.4
    
    * v1.4.5
    
    * v1.4.6
    
    * exports
    
    * v1.4.7
    
    * v1.4.8
    
    * added recent episodes to marin

--- src/providers/anime/marin.ts
+++ src/providers/anime/marin.ts
@@ -38,6 +38,41 @@ class Marin extends AnimeParser {
     return token;
   }
 
+  public recentEpisodes = async (page: number = 1): Promise<ISearch<IAnimeResult>> => {
+    let token = await this.getToken()
+    let data;
+    try {
+      let response = await axios.post('https://marin.moe/anime',{"page" : page,"sort":"rel-d","filter":{"type":[],"status":[],"content_rating":[],"genre":[],"group":[],"production":[],"source":[],"resolution":[],"audio":[],"subtitle":[]},"search": ""}, {
+        headers: {
+          Origin: 'https://marin.moe/',
+          Referer: 'https://marin.moe/anime',
+          Cookie: `__ddg1=;__ddg2_=; XSRF-TOKEN=${token[1]}; marinmoe_session=${token[0]};`,
+          'User-Agent':
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+          "x-xsrf-token": token[1].split(';')[0].replace("%3D", "="),
+          "x-inertia": true
+        },
+      });
+      data = await response.data;
+    } catch (error) {
+      console.log(error)
+    }
+    let response_data = {
+      currentPage: page,
+      hasNextPage: data.props.anime_list.meta.last_page > page,
+      results: data.props.anime_list.data.map((el: any) => {
+        return {
+          id: el.slug,
+          title: el.title,
+          image: el.cover,
+          releaseDate: el.year,
+          type: el.type
+        };
+      })
+    }
+    return response_data;
+  };
+
   /**
    * @param query Search query
    */
@@ -149,7 +184,7 @@ class Marin extends AnimeParser {
       ageRating: data.props.anime.content_rating.name,
       episodes: episodes.map((el: any) => {
         return {
-          id: el.sort,
+          id: `${id}/${el.sort}`,
           title: el.title,
           number: el.sort,
           image: el.cover,
@@ -213,7 +248,7 @@ class Marin extends AnimeParser {
 
 export default Marin;
 
-// (async () => {
-//   let marin = new Marin();
-//   console.log(await marin.fetchEpisodeSources('dewhzcns', 1));
-// })();
+(async () => {
+  let marin = new Marin();
+  console.log(await marin.recentEpisodes(1));
+})();
