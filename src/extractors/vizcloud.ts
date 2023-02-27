77a316a40eda32e30bb789d9d6565f59c32d92bb
Eltik <76538547+Eltik@users.noreply.github.com>
2023-02-27 12:22:43 -0800

    Update vizcloud.ts

--- src/extractors/vizcloud.ts
+++ src/extractors/vizcloud.ts
@@ -44,9 +44,24 @@ class VizCloud extends VideoExtractor {
       ...this.sources,
       ...data.data.media.sources.map((source: any) => ({
         url: source.file,
+        quality: 'auto',
         isM3U8: source.file?.includes('.m3u8'),
       })),
     ];
+
+    const main = this.sources[this.sources.length - 1].url;
+    const req = await axios.get(main);
+    const resolutions = req.data.match(/(RESOLUTION=)(.*)(\s*?)(\s*.*)/g);
+    resolutions?.forEach((res: string) => {
+        const index = main.lastIndexOf('/');
+        const quality = res.split('\n')[0].split('x')[1].split(',')[0];
+        const url = main.slice(0, index);
+        this.sources.push({
+            url: url + '/' + res.split('\n')[1],
+            isM3U8: (url + res.split('\n')[1]).includes('.m3u8'),
+            quality: quality + 'p',
+        });
+    });
     return this.sources;
   };
 }
