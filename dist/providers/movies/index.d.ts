7a6787244fd2637e581b0b18bdbfa3c000f2e771
soitchu <soitchu@proton.me>
2023-03-12 23:06:20 -0700

    Build

--- dist/providers/movies/index.d.ts
+++ dist/providers/movies/index.d.ts
@@ -1,9 +1,11 @@
 import FlixHQ from './flixhq';
 import ViewAsian from './viewAsian';
 import DramaCool from './dramacool';
+import Fmovies from './fmovies';
 declare const _default: {
     FlixHQ: typeof FlixHQ;
     ViewAsian: typeof ViewAsian;
     DramaCool: typeof DramaCool;
+    Fmovies: typeof Fmovies;
 };
 export default _default;
