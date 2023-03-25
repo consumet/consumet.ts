25dbe0ef7ccb245cecede154794ff603cef38c4d
Artikronisz <5531628+Artikronisz@users.noreply.github.com>
2023-03-25 16:05:07 -0400

    Build latest version

--- dist/extractors/vidcloud.js
+++ dist/extractors/vidcloud.js
@@ -5,7 +5,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
 const crypto_js_1 = __importDefault(require("crypto-js"));
-// import WebSocket from 'ws';
 const models_1 = require("../models");
 const utils_1 = require("../utils");
 class VidCloud extends models_1.VideoExtractor {
@@ -33,14 +32,14 @@ class VidCloud extends models_1.VideoExtractor {
                 let res = undefined;
                 let sources = undefined;
                 res = await axios_1.default.get(`${isAlternative ? this.host2 : this.host}/ajax/embed-4/getSources?id=${id}`, options);
-                //const res = await this.wss(id!);
                 if (!(0, utils_1.isJson)(res.data.sources)) {
                     let { data: key } = await axios_1.default.get('https://github.com/enimax-anime/key/blob/e4/key.txt');
                     key = (0, utils_1.substringBefore)((0, utils_1.substringAfter)(key, '"blob-code blob-code-inner js-file-line">'), '</td>');
                     if (!key) {
                         key = await (await axios_1.default.get('https://raw.githubusercontent.com/enimax-anime/key/e4/key.txt')).data;
                     }
-                    sources = JSON.parse(crypto_js_1.default.AES.decrypt(res.data.sources, key).toString(crypto_js_1.default.enc.Utf8));
+                    const decryptedVal = crypto_js_1.default.AES.decrypt(res.data.sources, key).toString(crypto_js_1.default.enc.Utf8);
+                    sources = (0, utils_1.isJson)(decryptedVal) ? JSON.parse(decryptedVal) : res.data.sources;
                 }
                 this.sources = sources.map((s) => ({
                     url: s.file,
@@ -82,28 +81,6 @@ class VidCloud extends models_1.VideoExtractor {
                 throw err;
             }
         };
-        // private wss = async (iframeId: string): Promise<any> => {
-        //   const ws = new WebSocket('wss://wsx.dokicloud.one/socket.io/?EIO=4&transport=websocket');
-        //   ws.onopen = () => {
-        //     ws.send('40');
-        //   };
-        //   return await new Promise(resolve => {
-        //     let sid = '';
-        //     let res: { sid: string; sources: any[]; tracks: any[] } = { sid: '', sources: [], tracks: [] };
-        //     ws.onmessage = e => {
-        //       const data = e.data.toString();
-        //       if (data.startsWith('40')) {
-        //         res.sid = JSON.parse(data.slice(2)).sid;
-        //         ws.send(`42["getSources",{"id":"${iframeId}"}]`);
-        //       } else if (data.startsWith('42["getSources"')) {
-        //         const ress = JSON.parse(data.slice(2))[1];
-        //         res.sources = ress.sources;
-        //         res.tracks = ress.tracks;
-        //         resolve(res);
-        //       }
-        //     };
-        //   });
-        // };
     }
 }
 exports.default = VidCloud;
