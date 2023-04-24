23e7534f1495a43017ba6d5c9325dd44c0b222af
XenDev PH <adagunan11@gmail.com>
2023-04-24 17:28:20 +0800

    feat: added asurascans provider

--- src/providers/manga/index.ts
+++ src/providers/manga/index.ts
@@ -6,5 +6,6 @@ import Mangasee123 from './mangasee123';
 import Mangapark from './mangapark';
 import MangaPill from './mangapill';
 import MangaReader from './mangareader';
+import AsuraScans from './asurascans';
 
-export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader };
+export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader, AsuraScans };
