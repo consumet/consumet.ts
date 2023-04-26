52031bcf21eed56202b6180729f9dceedb789d5e
XenDev PH <xendevph@gmail.com>
2023-04-26 17:10:49 +0800

    feat: added new provider (Flamescans)

--- src/providers/manga/index.ts
+++ src/providers/manga/index.ts
@@ -6,5 +6,6 @@ import Mangasee123 from './mangasee123';
 import Mangapark from './mangapark';
 import MangaPill from './mangapill';
 import MangaReader from './mangareader';
+import FlameScans from './flamescans';
 
-export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader };
+export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader, FlameScans };
