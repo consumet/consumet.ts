7431df202db95a5eb0d1196d63166e4ad2df6758
XenDevPH <57160276+XenriseZPH@users.noreply.github.com>
2023-04-30 03:04:00 +0800

    Added provider Flamescans (#334)
    * feat: added new provider (Flamescans)
    
    * chore: update providers list
    
    ---------
    
    Co-authored-by: XenDev PH <xendevph@gmail.com>
    Co-authored-by: riimuru <mythhb@yahoo.com>
    Co-authored-by: Marouane <57333995+riimuru@users.noreply.github.com>

--- src/providers/manga/index.ts
+++ src/providers/manga/index.ts
@@ -7,5 +7,6 @@ import Mangapark from './mangapark';
 import MangaPill from './mangapill';
 import MangaReader from './mangareader';
 import AsuraScans from './asurascans';
+import FlameScans from './flamescans';
 
-export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader, AsuraScans };
+export default { MangaDex, ComicK, MangaHere, MangaKakalot, Mangasee123, Mangapark, MangaPill, MangaReader, AsuraScans, FlameScans };
