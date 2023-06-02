31c490eff9cbb4b21d8b7851457668a76094a6c5
unknown <saulo.joab@bekagm.com>
2023-06-01 21:35:32 -0300

    feat: fixing test

--- test/manga/mangahost.test.ts
+++ test/manga/mangahost.test.ts
@@ -6,13 +6,6 @@ const mangahost = new MANGA.MangaHost();
 
 test('returns a filled array of manga', async () => {
   const data = await mangahost.search('punpun');
-  console.log(data.results);
-
-  const manga = await mangahost.fetchMangaInfo('oyasumi-punpun-mh34076');
-  console.log(manga);
-
-  const chapterImages = await mangahost.fetchChapterPages('oyasumi-punpun-mh34076', '1');
-  console.log(chapterImages);
 
   expect(data.results).not.toEqual([]);
 });
