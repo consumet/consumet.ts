920e9dc7f8468bc59333676c6a0ecf69e2cb2a8a
Saulo Joab <37988252+saulojoab@users.noreply.github.com>
2023-06-03 10:24:36 -0300

    feat: adding brmanga to providers list file

--- src/utils/providers-list.ts
+++ src/utils/providers-list.ts
@@ -28,6 +28,7 @@ export const PROVIDERS_LIST = {
     new MANGA.ComicK(),
     new MANGA.FlameScans(),
     new MANGA.MangaHost(),
+    new MANGA.BRMangas(),
   ],
   BOOKS: [new BOOKS.Libgen()],
   COMICS: [new COMICS.GetComics()],
