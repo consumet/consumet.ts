e60d80837a7b07654b5e5a864096e7a359bf609f
Raghav Mishra <raghavmishra1729@outlook.com>
2023-07-11 21:48:15 +0530

    Update mixdrop

--- src/extractors/mixdrop.ts
+++ src/extractors/mixdrop.ts
@@ -11,18 +11,10 @@ class MixDrop extends VideoExtractor {
     try {
       const { data } = await axios.get(videoUrl.href);
 
-      const match = load(data)
-        .html()
-        .match(/p}(.+?)wurl.+?}/g);
-
-      if (!match) {
-        throw new Error('Video not found.');
-      }
-      const [p, a, c, k, e, d] = match[0].split(',').map(x => x.split('.sp')[0]);
-      const formated = this.format(p, a, c, k, e, JSON.parse(d));
+      const formated = eval(/(eval)(\(f.*?)(\n<\/script>)/s.exec(data)![2].replace('eval', ''));
 
       const [poster, source] = formated
-        .match(/poster'="([^"]+)"|wurl="([^"]+)"/g)
+        .match(/poster="([^"]+)"|wurl="([^"]+)"/g)
         .map((x: string) => x.split(`="`)[1].replace(/"/g, ''))
         .map((x: string) => (x.startsWith('http') ? x : `https:${x}`));
 
@@ -36,33 +28,6 @@ class MixDrop extends VideoExtractor {
     } catch (err) {
       throw new Error((err as Error).message);
     }
-  };
-
-  private format = (p: any, a: any, c: any, k: any, e: any, d: any) => {
-    k = k.split('|');
-    e = (c: any) => {
-      return c.toString(36);
-    };
-    if (!''.replace(/^/, String)) {
-      while (c--) {
-        d[c.toString(a)] = k[c] || c.toString(a);
-      }
-      k = [
-        (e: any) => {
-          return d[e];
-        },
-      ];
-      e = () => {
-        return '\\w+';
-      };
-      c = 1;
-    }
-    while (c--) {
-      if (k[c]) {
-        p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]);
-      }
-    }
-    return p;
   };
 }
 export default MixDrop;
