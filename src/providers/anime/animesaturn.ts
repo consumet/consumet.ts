dda7dc70341fa9107d2e6588f275fb089dc631d8
Alessio Ganzarolli <ganzarolli.alessio@gmail.com>
2023-09-18 17:43:11 +0200

    feat(AnimeSaturn): streamtape sources (#387)
    * implement mp4 files scraping for AnimeSaturn
    
    implement mp4 files scraping (only m3u8 files were handled)
    add animesaturn api test
    add animesaturn api docs
    
    * Update animesaturn.md
    
    * add streamtape sources for AnimeSaturn provider
    
    * [CodeFactor] Apply fixes
    
    * add fetchable malID and alID from fetchAnimeInfo
    
    ---------
    
    Co-authored-by: codefactor-io <support@codefactor.io>

--- src/providers/anime/animesaturn.ts
+++ src/providers/anime/animesaturn.ts
@@ -9,7 +9,11 @@ import {
   ISource,
   IAnimeEpisode,
   IEpisodeServer,
+  StreamingServers,
 } from '../../models';
+
+import { StreamTape } from '../../utils';
+import { Mp4Player } from '../../extractors';
 
 class AnimeSaturn extends AnimeParser {
   override readonly name = 'AnimeSaturn';
@@ -63,6 +67,8 @@ class AnimeSaturn extends AnimeParser {
     const info: IAnimeInfo = {
       id,
       title: $('div.container.anime-title-as> b').text(),
+      malID: $('a[href^="https://myanimelist.net/anime/"]').attr('href')?.slice(30, -1),
+      alID: $('a[href^="https://anilist.co/anime/"]').attr('href')?.slice(25, -1),
       genres:
         $('div.container a.badge.badge-light')
           ?.map((i, element): string => {
@@ -106,12 +112,12 @@ class AnimeSaturn extends AnimeParser {
   override fetchEpisodeSources = async (episodeId: string): Promise<ISource> => {
     const fakeData = await this.client.get(`${this.baseUrl}ep/${episodeId}`);
     const $2 = await load(fakeData.data);
-    
-    const newUrl = $2("div > a:contains('Streaming')").attr('href');
-    
-    if (newUrl == null) throw new Error('Invalid url');
-    const data = await this.client.get(newUrl);
-    const $ = await load(data.data);
+
+    const serverOneUrl = $2("div > a:contains('Streaming')").attr('href'); // scrape from server 1 (m3u8 and mp4 urls)
+    if (serverOneUrl == null) throw new Error('Invalid url');
+
+    let data = await this.client.get(serverOneUrl);
+    let $ = await load(data.data);
     
     const sources: ISource = {
         headers: {},
@@ -119,41 +125,57 @@ class AnimeSaturn extends AnimeParser {
         sources: [],
     };
     
+    // M3U8 and MP4
     const scriptTag = $('script').filter(function () {
         return $(this).text().includes("jwplayer('player_hls')");
     });
 
-    let getOneSource: string | undefined;
+    let serverOneSource: string | undefined;
 
     // m3u8
     scriptTag.each((i, element) => {
       const scriptText = $(element).text();
 
       scriptText.split('\n').forEach(line => {
-        if (line.includes('file:') && !getOneSource) {
-          getOneSource = line.split('file:')[1].trim().replace(/'/g, '').replace(/,/g, '').replace(/"/g, '');
+        if (line.includes('file:') && !serverOneSource) {
+          serverOneSource = line.split('file:')[1].trim().replace(/'/g, '').replace(/,/g, '').replace(/"/g, '');
         }
       });
     });
 
     // mp4
-    if (!getOneSource) {
-        getOneSource =  $('#myvideo > source').attr('src')
+    if (!serverOneSource) {
+        serverOneSource =  $('#myvideo > source').attr('src')
     }
 
-    if (!getOneSource) throw new Error('Invalid source');
+    if (!serverOneSource) throw new Error('Invalid source');
 
     sources.sources.push({
-      url: getOneSource,
-      isM3U8: getOneSource.includes('.m3u8'),
+      url: serverOneSource,
+      isM3U8: serverOneSource.includes('.m3u8'),
     });
 
-    if (getOneSource.includes('.m3u8')) {    
+    if (serverOneSource.includes('.m3u8')) {    
         sources.subtitles?.push({
-          url: getOneSource.replace('playlist.m3u8', 'subtitles.vtt'),
+          url: serverOneSource.replace('playlist.m3u8', 'subtitles.vtt'),
           lang: 'Spanish',
         });
     }
+
+    // STREAMTAPE
+    const serverTwoUrl = serverOneUrl + '&server=1' // scrape from server 2 (streamtape)
+    data = await this.client.get(serverTwoUrl);
+    $ = await load(data.data);
+
+    const videoUrl = $('.embed-container > iframe').attr('src');
+    const serverTwoSource = await new StreamTape(this.proxyConfig, this.adapter).extract(new URL(videoUrl!));
+
+    if (!serverTwoSource) throw new Error('Invalid source');
+
+    sources.sources.push({
+        url: serverTwoSource[0].url,
+        isM3U8: serverTwoSource[0].isM3U8,
+    });
 
     return sources;
   };
