167dc41a85fccfdabb445cda1e1156681c887186
Jason Spalti <vipershniper08@gmail.com>
2024-02-17 05:58:18 -0600

    Update vidcloud.ts

--- src/extractors/vidcloud.ts
+++ src/extractors/vidcloud.ts
@@ -36,14 +36,11 @@ class VidCloud extends VideoExtractor {
       );
 
       if (!isJson(res.data.sources)) {
-        const keys = await (await this.client.get('https://keys4.fun')).data["rabbitstream"]["keys"];
-                    const keyString = btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(keys))));
-                    const decryptedVal = CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8);
-                    sources = JSON.parse(CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8));        
-
-
-        sources = isJson(decryptedVal) ? JSON.parse(decryptedVal) : res.data.sources;
-        
+           let keys = await (await this.client.get('https://raw.githubusercontent.com/eatmynerds/key/e4/key.txt')).data;
+           const keyString = btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(JSON.parse(keys)))));
+           const decryptedVal = CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8);
+           sources = JSON.parse(CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8)); 
+           sources = isJson(decryptedVal) ? JSON.parse(decryptedVal) : res.data.sources;
       }
 
       this.sources = sources.map((s: any) => ({
