397872425a0e728725f8347461a8678f7c70e2e7
Gabriel De Castro <decastro.gabriel00@gmail.com>
2024-02-23 08:04:33 +0800

    refactor(gogoanime): Optimized code block for anime list

--- src/providers/anime/gogoanime.ts
+++ src/providers/anime/gogoanime.ts
@@ -481,11 +481,7 @@ class Gogoanime extends AnimeParser {
     try {
       res = await this.client.get(`${this.baseUrl}/anime-list.html?page=${page}`);
     } catch (err) {
-      try {
-        res = await this.client.get(`${this.baseUrl}/`);
-      } catch (error) {
-        throw new Error('Something went wrong. Please try again later.');
-      }
+      throw new Error('Something went wrong. Please try again later.');
     }
     try {
       const $ = load(res.data);
