64a8ceccb0c2ccf9102402eb533c650c0f3a67d4
Gabriel De Castro <37971981+hase0278@users.noreply.github.com>
2024-03-29 19:07:47 +0800

    feat(gogoanime): add ability to customize recaptcha token fetchDirectDownloadLink

--- dist/providers/anime/gogoanime.js
+++ dist/providers/anime/gogoanime.js
@@ -410,7 +410,7 @@ class Gogoanime extends models_1.AnimeParser {
             const baseUrl = downloadUrl.split('?')[0];
             const idParam = downloadUrl.match(/[?&]id=([^&]+)/);
             const animeID = idParam ? idParam[1] : null;
-            if (captchaToken = '') {
+            if (captchaToken == '') {
                 captchaToken = '03AFcWeA5zy7DBK82U_tctVKelJ6L2duTWac5at2zXjHLX8XqUm8tI6NKWMxGd2gjh1vi2hnEyRhVgbMhdb9WjexRsJkxTt-C-_iIIZ5yC3E5I19G5Q0buSTcIQIZS6tskrz-mDn-d37aWxAJtqbg0Yoo1XsdVc5Yf4sB-9iQxQK-W_9YLep_QaAz8uL17gMMlCz5WZM3dbBEEGmk_qPbJu_pZ8kk-lFPDzd6iBobcpyIDRZgTgD4bYUnby5WZc11i00mrRiRS3m-qSY0lprGaBqoyY1BbRkQZ25AGPp5al4kSwBZqpcVgLrs3bjdo8XVWAe73_XLa8HhqLWbz_m5Ebyl5F9awwL7w4qikGj-AK7v2G8pgjT22kDLIeenQ_ss4jYpmSzgnuTItur9pZVzpPkpqs4mzr6y274AmJjzppRTDH4VFtta_E02-R7Hc1rUD2kCYt9BqsD7kDjmetnvLtBm97q5XgBS8rQfeH4P-xqiTAsJwXlcrPybSjnwPEptqYCPX5St_BSj4NQfSuzZowXu_qKsP4hAaE9L2W36MvqePPlEm6LChBT3tnqUwcEYNe5k7lkAAbunxx8q_X5Q3iEdcFqt9_0GWHebRBd5abEbjbmoqqCoQeZt7AUvkXCRfBDne-bf25ypyTtwgyuvYMYXau3zGUjgPUO9WIotZwyKyrYmjsZJ7TiM';
             }
             let res = null;
