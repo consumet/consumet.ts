f274db0bd08f0f3808a1af140324c5e9ae3019e2
codefactor-io <support@codefactor.io>
2024-06-02 15:43:55 +0000

    [CodeFactor] Apply fixes to commit 8345a5a

--- test/movies/vidcloud.test.ts
+++ test/movies/vidcloud.test.ts
@@ -3,13 +3,13 @@ import { VidCloud } from '../../src/extractors';
 
 async function main() {
   console.log("test start");
-  let vidCloud = new VidCloud();
+  const vidCloud = new VidCloud();
   console.log("vidCloud object created!");
-  let url = new URL("https://rabbitstream.net/v2/embed-4/x21Ne4cJOFWC?z=");
+  const url = new URL("https://rabbitstream.net/v2/embed-4/x21Ne4cJOFWC?z=");
   console.log("url created!");
   console.log("extract called!");
   try {
-    let res = await vidCloud.extract(url);
+    const res = await vidCloud.extract(url);
     console.log("extract success!")
     console.log(res);
   } catch (error) {
