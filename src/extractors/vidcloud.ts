8345a5a662307ac8ec4ed4166a720cc5d7358bcd
drblgn <32202257+drblgn@users.noreply.github.com>
2024-06-02 18:43:44 +0300

    Rabbitfix (#537)
    * fix for rabbitstream
    * codefactor

--- src/extractors/vidcloud.ts
+++ src/extractors/vidcloud.ts
@@ -1,18 +1,15 @@
-import CryptoJS from 'crypto-js';
-
+import { main } from './rabbit'
 import { VideoExtractor, IVideo, ISubtitle, Intro } from '../models';
-import { USER_AGENT, isJson, substringAfter, substringBefore } from '../utils';
+import { USER_AGENT } from '../utils';
 
 class VidCloud extends VideoExtractor {
   protected override serverName = 'VidCloud';
   protected override sources: IVideo[] = [];
 
-  private readonly host = 'https://dokicloud.one';
-  private readonly host2 = 'https://rabbitstream.net';
 
   override extract = async (
     videoUrl: URL,
-    isAlternative: boolean = false
+    _?: boolean,
   ): Promise<{ sources: IVideo[] } & { subtitles: ISubtitle[] }> => {
     const result: { sources: IVideo[]; subtitles: ISubtitle[]; intro?: Intro } = {
       sources: [],
@@ -27,20 +24,9 @@ class VidCloud extends VideoExtractor {
           'User-Agent': USER_AGENT,
         },
       };
-      let res = undefined;
-      let sources = undefined;
 
-      res = await this.client.get(
-        `${isAlternative ? this.host2 : this.host}/ajax/embed-4/getSources?id=${id}`,
-        options
-      );
-
-      if (!isJson(res.data.sources)) {
-           const keys = await (await this.client.get('https://raw.githubusercontent.com/eatmynerds/key/e4/key.txt')).data;
-           const keyString = btoa(String.fromCharCode.apply(null, Array.from(new Uint8Array(JSON.parse(JSON.stringify(keys))))));
-           const decryptedVal = CryptoJS.AES.decrypt(res.data.sources, keyString).toString(CryptoJS.enc.Utf8); 
-           sources = isJson(decryptedVal) ? JSON.parse(decryptedVal) : res.data.sources;
-      }
+      const res = await main(id);
+      const sources = res.sources;
 
       this.sources = sources.map((s: any) => ({
         url: s.file,
@@ -80,7 +66,7 @@ class VidCloud extends VideoExtractor {
         quality: 'auto',
       });
 
-      result.subtitles = res.data.tracks.map((s: any) => ({
+      result.subtitles = res.tracks.map((s: any) => ({
         url: s.file,
         lang: s.label ? s.label : 'Default (maybe)',
       }));
