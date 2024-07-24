322ce34be57a595b931282a14c04c6aac8af7229
FeriVOQ <36544651+ferivoq@users.noreply.github.com>
2024-07-24 19:38:48 +0200

    Update test/anime/animedrive.test.ts
    Co-authored-by: Dmytro Laptiev <42392852+AkioSarkiz@users.noreply.github.com>

--- test/anime/animedrive.test.ts
+++ test/anime/animedrive.test.ts
@@ -7,7 +7,6 @@ const animedrive = new ANIME.AnimeDrive();
 test('returns a filled array of anime list', async () => {
   const animedrive = new ANIME.AnimeDrive();
   const data = await animedrive.search('demon');
-  // console.log(data);
   expect(data.results).not.toEqual([]);
 });
 
