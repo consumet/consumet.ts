008413c859b6314059a81a1ac589f6940a3195f7
codefactor-io <support@codefactor.io>
2024-09-21 13:49:34 +0000

    [CodeFactor] Apply fixes to commit e85f989

--- src/extractors/streamwish.ts
+++ src/extractors/streamwish.ts
@@ -30,22 +30,22 @@ class StreamWish extends VideoExtractor {
       // Code adapted from Zenda-Cross (https://github.com/Zenda-Cross/vega-app/blob/main/src/lib/providers/multi/multiGetStream.ts)
       // Thank you to Zenda-Cross for the original implementation.
 
-      var functionRegex =
+      const functionRegex =
         /eval\(function\((.*?)\)\{.*?return p\}.*?\('(.*?)'\.split/;
-      var match = functionRegex.exec(data);
+      const match = functionRegex.exec(data);
       let p = '';
       if (match) {
-        var params = match[1].split(',').map(param => param.trim());
-        var encodedString = match[2];
+        const params = match[1].split(',').map(param => param.trim());
+        const encodedString = match[2];
 
         p = encodedString.split("',36,")?.[0].trim();
-        let a = 36;
+        const a = 36;
         let c = encodedString.split("',36,")[1].slice(2).split('|').length;
-        let k = encodedString.split("',36,")[1].slice(2).split('|');
+        const k = encodedString.split("',36,")[1].slice(2).split('|');
 
         while (c--) {
           if (k[c]) {
-            var regex = new RegExp('\\b' + c.toString(a) + '\\b', 'g');
+            const regex = new RegExp('\\b' + c.toString(a) + '\\b', 'g');
             p = p.replace(regex, k[c]);
           }
         }
