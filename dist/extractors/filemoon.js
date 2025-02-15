309619eedb2c292c383cf66a452199401613360f
Gabriel De Castro <decastro.gabriel00@gmail.com>
2025-02-16 00:28:19 +0800

    fix(filemoon): Update extractor logic to latest filemoon changes (#655)

--- dist/extractors/filemoon.js
+++ dist/extractors/filemoon.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const cheerio_1 = require("cheerio");
 const models_1 = require("../models");
 const utils_1 = require("../utils");
 /**
@@ -12,20 +13,41 @@ class Filemoon extends models_1.VideoExtractor {
         this.sources = [];
         this.host = 'https://filemoon.sx';
         this.extract = async (videoUrl) => {
+            var _a;
             const options = {
                 headers: {
-                    Referer: videoUrl.href,
-                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                    'Accept-Language': 'en-US,en;q=0.9',
+                    Cookie: 'file_id=40342338; aff=23788; ref_url=https%3A%2F%2Fbf0skv.org%2Fe%2Fm0507zf4xqor; lang=1',
+                    Priority: 'u=0, i',
+                    Referer: videoUrl.origin,
+                    Origin: videoUrl.href,
+                    'Sec-Ch-Ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+                    'Sec-Ch-Ua-Mobile': '?0',
+                    'Sec-Ch-Ua-Platform': '"Windows"',
+                    'Sec-Fetch-Dest': 'iframe',
+                    'Sec-Fetch-Mode': 'navigate',
+                    'Sec-Fetch-Site': 'cross-site',
                     'User-Agent': utils_1.USER_AGENT,
-                    'X-Requested-With': 'XMLHttpRequest',
+                    'Access-Control-Allow-Origin': '*',
                 },
             };
-            const { data } = await this.client.get(videoUrl.href);
-            const s = data.substring(data.indexOf('eval(function') + 5, data.lastIndexOf(')))'));
+            const { data } = await this.client.get(videoUrl.href, options);
+            const $ = (0, cheerio_1.load)(data);
             try {
-                const newScript = 'function run(' + s.split('function(')[1] + '))';
+                const { data } = await this.client.get($('iframe').attr('src'), options);
+                const unpackedData = eval(/(eval)(\(f.*?)(\n<\/script>)/s.exec(data)[2].replace('eval', ''));
+                const links = (_a = unpackedData.match(new RegExp('sources:\\[\\{file:"(.*?)"'))) !== null && _a !== void 0 ? _a : [];
+                const m3u8Link = links[1];
+                this.sources.unshift({
+                    url: m3u8Link,
+                    quality: 'auto',
+                    isM3U8: true,
+                });
             }
-            catch (err) { }
+            catch (err) {
+                console.log(err);
+            }
             return this.sources;
         };
     }
--- src/extractors/filemoon.ts
+++ src/extractors/filemoon.ts
@@ -1,6 +1,6 @@
 import { load } from 'cheerio';
 
-import { VideoExtractor, IVideo, ISubtitle, Intro } from '../models';
+import { VideoExtractor, IVideo, ISubtitle, Intro, ISource } from '../models';
 import { USER_AGENT } from '../utils';
 import { Console } from 'console';
 
@@ -16,19 +16,39 @@ class Filemoon extends VideoExtractor {
   override extract = async (videoUrl: URL): Promise<IVideo[]> => {
     const options = {
       headers: {
-        Referer: videoUrl.href,
-        'Content-Type': 'application/x-www-form-urlencoded',
+        Accept:
+          'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Language': 'en-US,en;q=0.9',
+        Cookie: 'file_id=40342338; aff=23788; ref_url=https%3A%2F%2Fbf0skv.org%2Fe%2Fm0507zf4xqor; lang=1',
+        Priority: 'u=0, i',
+        Referer: videoUrl.origin,
+        Origin: videoUrl.href,
+        'Sec-Ch-Ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Platform': '"Windows"',
+        'Sec-Fetch-Dest': 'iframe',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'cross-site',
         'User-Agent': USER_AGENT,
-        'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Origin': '*',
       },
     };
 
-    const { data } = await this.client.get(videoUrl.href);
-
-    const s = data.substring(data.indexOf('eval(function') + 5, data.lastIndexOf(')))'));
+    const { data } = await this.client.get(videoUrl.href, options);
+    const $ = load(data);
     try {
-      const newScript = 'function run(' + s.split('function(')[1] + '))';
-    } catch (err) {}
+      const { data } = await this.client.get($('iframe').attr('src')!, options)!;
+      const unpackedData = eval(/(eval)(\(f.*?)(\n<\/script>)/s.exec(data)![2].replace('eval', ''));
+      const links = unpackedData.match(new RegExp('sources:\\[\\{file:"(.*?)"')) ?? [];
+      const m3u8Link = links[1];
+      this.sources.unshift({
+        url: m3u8Link,
+        quality: 'auto',
+        isM3U8: true,
+      });
+    } catch (err) {
+      console.log(err);
+    }
     return this.sources;
   };
 }
