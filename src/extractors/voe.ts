3b3edb2861e6d1fbbe045245b419dd4e6dda581d
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-03-01 21:52:21 +0530

    fix(voe): minor encryption fix

--- src/extractors/voe.ts
+++ src/extractors/voe.ts
@@ -19,7 +19,9 @@ class Voe extends VideoExtractor {
 
       const { data } = await this.client.get(pageUrl);
       const $$ = load(data);
-      const url = $$('body').html()!.split('prompt("Node", "')[1].split('");')[0];
+      const url = $$('body')
+        .html()!
+        .match(/'hls'\s*:\s*'([^']+)'/s)?.[1]!;
 
       let thumbnailSrc: string = '';
       $$('script').each((i, el) => {
@@ -41,9 +43,9 @@ class Voe extends VideoExtractor {
       ];
 
       this.sources.push({
-        url: url,
+        url: atob(url),
         quality: 'default',
-        isM3U8: url.includes('.m3u8'),
+        isM3U8: atob(url).includes('.m3u8'),
       });
 
       return {
