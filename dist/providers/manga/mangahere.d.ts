45f047f57b6e5abbf6131d605d2e816dc93ccbb4
Ilya Bogdanov <34226834+skirkru@users.noreply.github.com>
2025-11-25 21:43:10 +0300

    feat(provider): MangaHere (#728)
    small fix

--- dist/providers/manga/mangahere.d.ts
+++ dist/providers/manga/mangahere.d.ts
@@ -1,4 +1,7 @@
 import { MangaParser, ISearch, IMangaInfo, IMangaResult, IMangaChapterPage } from '../../models';
+export type MangaHereRankingType = 'total' | 'month' | 'week' | 'day';
+export type MangaHereSort = 'az' | 'popularity' | 'rating' | 'latest' | 'news';
+export type MangaHereStatus = 'all' | 'new' | 'completed' | 'ongoing';
 declare class MangaHere extends MangaParser {
     readonly name = "MangaHere";
     protected baseUrl: string;
@@ -7,6 +10,17 @@ declare class MangaHere extends MangaParser {
     fetchMangaInfo: (mangaId: string) => Promise<IMangaInfo>;
     fetchChapterPages: (chapterId: string) => Promise<IMangaChapterPage[]>;
     search: (query: string, page?: number) => Promise<ISearch<IMangaResult>>;
+    fetchMangaRanking: (type?: MangaHereRankingType) => Promise<ISearch<IMangaResult>>;
+    fetchMangaHotReleases: () => Promise<ISearch<IMangaResult>>;
+    fetchMangaTrending: () => Promise<ISearch<IMangaResult>>;
+    fetchMangaRecentUpdate: (page?: number) => Promise<ISearch<IMangaResult>>;
+    browse: (options?: {
+        genre?: string;
+        status?: MangaHereStatus;
+        sort?: MangaHereSort;
+        page?: number;
+    }) => Promise<ISearch<IMangaResult>>;
+    private _getMangaList;
     /**
      *  credit: [tachiyomi-extensions](https://github.com/tachiyomiorg/tachiyomi-extensions/blob/master/src/en/mangahere/src/eu/kanade/tachiyomi/extension/en/mangahere/Mangahere.kt)
      */
